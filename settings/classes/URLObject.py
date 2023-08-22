95052d95e6ebf5d62a718140fbc95c77bc991e41
burov-kirill <burovkiriill1995@gmail.com>
2023-08-22 23:13:30 +0800

    update presentation function

--- settings/classes/URLObject.py
+++ settings/classes/URLObject.py
@@ -229,9 +229,7 @@ class GoogleDocFile:
         succeed_table.reset_index(inplace=True)
         succeed_table.columns = ['Company', 'Type', 'Coeff']
         worst_company = succeed_table[succeed_table['Type'] == 'ТЗО'].sort_values(by='Coeff').iloc[:10][['Company', 'Coeff']]
-        worst_company['Coeff'] = worst_company['Coeff']*100
         best_company = succeed_table[succeed_table['Type'] != 'ТЗО'].sort_values(by='Coeff').iloc[-11:-1][['Company', 'Coeff']]
-        best_company['Coeff'] = best_company['Coeff'] * 100
         average_table = pd.DataFrame(map(lambda x: (x[0], sum(x[1])/len(x[1])), self.AVERAGE_POINT_DICT.items()), columns = ['Участок','Средний бал'])
         average_table = average_table.sort_values(by=['Средний бал'], ascending=True)
         main_table = pd.concat([main_table, summary_row])
